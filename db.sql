c011732c9d74b1b5cd5aca2ecc1c71236c16dd9f
Josep Andreu <jandreu@externos.itnow.es>
2021-09-20 17:29:42 +0200

    Added db files

---
+++ db.sql
@@ -0,0 +1,3 @@
+CREATE TABLE Projects (id int NOT NULL, name varchar(255) DEFAULT NULL, code varchar(255) DEFAULT NULL, PRIMARY KEY (id));
+
+insert into Projects (id, name, code) values (1,'DevOps','Cloud');
